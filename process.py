ae6c24510b9bad492aee4dfc52231014fd9ceef2
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-10-13 17:16:57 -0500

    Added classes to encrypt and decrypt messages

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 # process.py
 #
-# describe this file!!!
+# contains methods for encrypting and decrypting text with a private key
 
 # -------------------------------------------------------------------------------------------
 
@@ -13,6 +13,7 @@ import sys
 # main
 ### allows use of this file's encryption and decryption methods from the command line
 def main():
+
     # running loop
     while True:
         # prompt to either encrypt or decrypt
@@ -25,13 +26,15 @@ def main():
             print("\nEnter message to encrypt:")
             message = str(input())
             
-            # generates and prints key
-            key = Fernet.generate_key()
-            print("\nThank you. Generating key...\n\nYour generated key is:\n" + key.decode('utf-8') + "\n\nSave this key to be able to decrypt your message.")
+            # encrypts message
+            encrypted_message = EncryptedMessage(message)
+
+            # prints key
+            key = encrypted_message.getKey()
+            print("\nGenerating key...\n\nYour generated key is:\n" + key.decode('utf-8') + "\n\nSave this key to be able to decrypt your message.")
             
-            # encrypts message and prints encrypted message
-            encrypted_message = encryptMessage(message, key)
-            print("\nYour encrypted message is:\n" + encrypted_message.decode("utf-8"))
+            # prints encrypted message
+            print("\nYour encrypted message is:\n" + encrypted_message.getString())
         
         # if decrypt...
         elif encrypt_or_decrypt == "D":
@@ -39,13 +42,15 @@ def main():
             print("\nEnter message to decrypt:")
             message = str(input())
             
-            # generates key or gets key from user
-            print("\nThank you.\n\nPlease provide the key used to encrypt the message:")
+            # gets key from user
+            print("\nPlease provide the key used to encrypt the message:")
             key = str(input())
             
-            # decrypts message and prints response
-            decrypted_message = decryptMessage(message, key)
-            print("\nYour decrypted message is:\n" + decrypted_message)
+            # decrypts message
+            decrypted_message = DecryptedMessage(message, key)
+
+            # prints decrypted message
+            print("\nYour decrypted message is:\n" + decrypted_message.getString())
 
         # if quit...
         elif encrypt_or_decrypt == "QUIT":
@@ -58,14 +63,119 @@ def main():
         
 # -------------------------------------------------------------------------------------------
 
+# EncryptedMessage
+# encrypts a provided message and stores it as a string, bytes, and a list of binary values
+### message: message to be encrypted; may be a string, bytes, or a list of binary values
+### self.key: generated encryption key
+### self.bytes_encrypted_msg: encrypted message as bytes
+### self.string_encrypted_msg: encrypted message as string
+### self.binlist_encrypted_msg: encrypted message as list of binary values
+class EncryptedMessage:
+    
+    # initialization
+    def __init__(self, message):
+        
+        # generate key
+        self.key = Fernet.generate_key()
+
+        # encrypt message and store encrypted versions
+        # DO NOT STORE ORIGINAL MESSAGE IN THIS CLASS
+        if str(type(message)) == str(type("")):
+            self.bytes_encrypted_msg = encryptMessage(message, self.key)
+            self.string_encrypted_msg = self.bytes_encrypted_msg.decode('utf-8')
+            self.binlist_encrypted_msg = stringToBinaryList(self.string_encrypted_msg)
+        elif str(type(message)) == str(type(b"")):
+            self.bytes_encrypted_msg = encryptMessage(message.decode('utf-8'), self.key)
+            self.string_encrypted_msg = self.bytes_encrypted_msg.decode('utf-8')
+            self.binlist_encrypted_msg = stringToBinaryList(self.string_encrypted_msg)
+        elif str(type(message)) == str(type([])):
+            self.bytes_encrypted_msg = encryptMessage(binaryListToString(message))
+            self.string_encrypted_msg = self.bytes_encrypted_msg.decode('utf-8')
+            self.binlist_encrypted_msg = stringToBinaryList(self.string_encrypted_msg)
+        else:
+            self.bytes_encrypted_msg = ""
+            self.string_encrypted_msg = ""
+            self.binlist_encrypted_msg = []
+
+    # get key
+    def getKey(self):
+        return self.key
+    
+    # get encrypted message as bytes
+    def getBytes(self):
+        return self.bytes_encrypted_msg
+
+    # get encrypted message as string
+    def getString(self):
+        return self.string_encrypted_msg
+
+    # get encrypted message as list of binary values
+    def getBinList(self):
+        return self.binlist_encrypted_msg
+
+# -------------------------------------------------------------------------------------------
+    
+# DecryptedMessage
+# decrypts a provided message and stores it as
+### message: message to be decrypted; may be a string, bytes, or a list of binary values
+### key: key to decrypt message
+### self.key: generated encryption key
+### self.bytes_encrypted_msg: encrypted message as bytes
+### self.string_encrypted_msg: encrypted message as string
+### self.binlist_encrypted_msg: encrypted message as list of binary values
+class DecryptedMessage:
+    
+    # initialization
+    def __init__(self, message, key):
+        
+        # store key
+        self.key = key
+
+        # decrypt message and store decrypted versions
+        if str(type(message)) == str(type("")):
+            self.bytes_decrypted_msg = decryptMessage(message, self.key)
+            self.string_decrypted_msg = self.bytes_decrypted_msg.decode('utf-8')
+            self.binlist_decrypted_msg = stringToBinaryList(self.string_decrypted_msg)
+        elif str(type(message)) == str(type(b"")):
+            self.bytes_decrypted_msg = decryptMessage(message.decode('utf-8'), self.key)
+            self.string_decrypted_msg = self.bytes_decrypted_msg.decode('utf-8')
+            self.binlist_encrypted_msg = stringToBinaryList(self.string_decrypted_msg)
+        elif str(type(message)) == str(type([])):
+            self.bytes_decrypted_msg = decryptMessage(binaryListToString(message))
+            self.string_decrypted_msg = self.bytes_decrypted_msg.decode('utf-8')
+            self.binlist_decrypted_msg = stringToBinaryList(self.string_decrypted_msg)
+        else:
+            self.bytes_decrypted_msg = ""
+            self.string_decrypted_msg = ""
+            self.binlist_decrypted_msg = []
+
+    # get key
+    def getKey(self):
+        return self.key
+    
+    # get decrypted message as bytes
+    def getBytes(self):
+        return self.bytes_decrypted_msg
+
+    # get decrypted message as string
+    def getString(self):
+        return self.string_decrypted_msg
+
+    # get decrypted message as list of binary values
+    def getBinList(self):
+        return self.binlist_decrypted_msg
+
+# -------------------------------------------------------------------------------------------
+
 # encryptMessage
 ### message: string to be encrypted
 ### key: encryption key, must be generated by Fernet
-### return: encryped message as bytes
+### return: encrypted message as bytes
 def encryptMessage(message, key):
+    
     # sanitize message
     sani_message = cleanMessage(str(message))
-    b_sani_message = (bytes(sani_message, 'utf-8'))
+    b_sani_message = bytes(sani_message, 'utf-8')
 
     # process key
     k = Fernet(key)
@@ -81,12 +191,12 @@ def encryptMessage(message, key):
 # decryptMessage
 ### message: string to be decrypted
 ### key: key used to encrypt the provided message
-### return: the decrypted message as a string
+### return: the decrypted message as bytes
 def decryptMessage(message, key):
-    # sanitize inputs
-    # sani_key = cleanMessage(key.decode('utf-8'))
+    
+    # sanitize message
     sani_message = cleanMessage(str(message))
-    b_sani_message = (bytes(sani_message, 'utf-8'))
+    b_sani_message = bytes(sani_message, 'utf-8')
 
     # process key
     k = Fernet(key)
@@ -95,14 +205,16 @@ def decryptMessage(message, key):
     decrypted_msg = k.decrypt(b_sani_message)
 
     # return decrypted message
-    return decrypted_msg.decode("utf-8")
+    return decrypted_msg
 
 # -------------------------------------------------------------------------------------------
 
+# cleanMessage
 # sanitizes a string OR terminates program if unacceptable characters are found in string
 ### message: string to be sanitized
 ### return: the sanitized string
 def cleanMessage(message):
+    
     # cast argument to string (precaution)
     message = str(message)
 
@@ -138,6 +250,38 @@ def cleanMessage(message):
 
     # return sanitized message
     return sani_message
+
+# -------------------------------------------------------------------------------------------
+
+
+# makes each item in a list of binary values 8 bits long
+def appendZeroes(binary_list):
+    binary_char_list = []
+    for str in binary_list:
+        while(len(str) < 8 ):
+            str = "0" + str
+        binary_char_list.append(str)
+    return binary_char_list
+
+# -------------------------------------------------------------------------------------------
+
+# converts string to a list of binary
+def stringToBinaryList(string):
+    binary_string = ' '.join(format(ord(i), 'b') for i in string)
+    binary_list = binary_string.split()
+    binary_char_list = appendZeroes(binary_list)
+    return binary_char_list
+
+# -------------------------------------------------------------------------------------------
+
+# converts a list of binary values to an ASCII string
+def binaryListToString(binary_list):
+    string = ""
+    for each in binary_list:
+        base2int = int(each, 2) # convert to base 2 int
+        ascii_char = chr(base2int) # convert to ascii char
+        string += ascii_char
+    return string
 
 # -------------------------------------------------------------------------------------------
 
