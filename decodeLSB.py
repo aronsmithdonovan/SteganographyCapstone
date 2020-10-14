13ff207d3e201c2e72d9b9edee1c5a4b93fbb281
Aron Smith-Donovan <aronsmithdonovan@gmail.com>
2020-10-13 20:41:30 -0500

    Now encrypt & work w multiple rows of pixels

---
+++ decodeLSB.py
@@ -0,0 +1,99 @@
+# encode.py
+# 
+# contains methods for encoding a message in an image
+# 
+# -------------------------------------------------------------------------------------------
+
+from PIL import Image
+from process import *
+
+# -------------------------------------------------------------------------------------------
+
+# main
+def main():
+    
+    # take key input
+    print("\nProvide the key used to encrypt the message:")
+    key = str(input())
+
+    # take image input
+        #print("Enter the name of the image file (include extension)")
+        #image_file = input()
+    image_file = "encodedCat.png"
+    image = Image.open(image_file, 'r')
+
+    # decode message from image with key
+    message = decode(image, key)
+
+    # print decoded message
+    print("\nYour decoded message is: " + message)
+
+# -------------------------------------------------------------------------------------------
+
+# decode
+# decodes a message encoded in an image using the encode function in encodeLSB.py
+### image: image with encoded message
+### key: key used to encode message
+### returns: message as string
+def decode(image, key):
+
+    # copies pixel data for input image
+    pixels = image.load()
+    
+    # initializes objects for zero counter
+    zero_stack = []
+    all_zeroes = "000000000"
+
+    # sets width and height values
+    width, height = image.size
+
+    # initializes variables for loop
+    w_index = 0
+    h_index = 0
+    bit_index = 0
+    bin_string = ""
+    bin_list = []
+    not_zero = True
+
+    while not_zero == True:
+    
+        # empty bin_string
+        bin_string = ""
+
+        # iterates through 3 pixels, gets least significant bit of RGB binary values
+        for count in range(3):
+            # get RGB values for current pixel
+            r, g, b = pixels[w_index, h_index]
+
+            # get least significant bit of each byte
+            bin_string += bin(r)[-1]
+            bin_string += bin(g)[-1]
+            if count < 2:
+                bin_string += bin(b)[-1]
+
+            # update w_index
+            if w_index == (width - 1):
+                w_index = 0
+                h_index += 1
+            else:
+                w_index += 1
+        
+        # check for zeroes
+        if int(bin_string, 2) == 0:
+            not_zero == False
+            break
+        
+        # save string value
+        bin_list.append(bin_string)
+
+    # decrypt message
+    message = DecryptedMessage(bin_list, key)
+
+    # return message
+    return message.getString()
+
+# -------------------------------------------------------------------------------------------
+
+# call main fxn
+if __name__ == '__main__' :
+    main()
---
+++ encodeLSB.py
@@ -0,0 +1,152 @@
+# encode.py
+# 
+# contains methods for encoding a message in an image
+# 
+# -------------------------------------------------------------------------------------------
+
+# imports
+from PIL import Image
+from process import *
+
+# -------------------------------------------------------------------------------------------
+
+# main
+# takes message input and encodes it in an image input
+def main():
+    
+    # take message input
+    print("\nEnter the message to encode:")
+    
+    # encrypt message
+    encrypted_msg = EncryptedMessage(str(input()))
+
+    # print key
+    key = encrypted_msg.getKey()
+    print("\nYour generated key is:\n" + key.decode('utf-8') + "\n\nSave this key to be able to decrypt your message.")
+            
+    # take image input
+        #print("Enter the name of the image file (include extension)")
+        #image_file = input()
+    image_file = "cat.png"
+    image = Image.open(image_file, 'r')
+
+    # encode encrypted message in image
+    enc_image = encode(image, encrypted_msg.getBinList())
+
+    # save encoded image
+    enc_image.save('encodedCat.png')
+
+    # display encoded image
+    enc_image.show()
+
+# -------------------------------------------------------------------------------------------
+
+# encode
+# encodes a list of binary values into an image with the LSB (least significant bit) approach
+### image: image object
+### bin_list: list of binary values to encode into an image
+def encode(image, data):
+    
+    # copies input image
+    enc_image = image.copy()
+    
+    # sets width and height values
+    width, height = enc_image.size
+
+    # copies pixel data for input image
+    pixels = enc_image.load()
+    
+    # initializes variables for loop
+    w_index = 0
+    h_index = 0
+    bit_index = 0
+    
+    # select 3 pixels at a time (right now only works on one line of image, so only shorter messages will works)
+    # print(pixels[0, 0])
+    for i in range(len(data)):
+        
+        # set bit_index to 0
+        bit_index = 0
+
+        # iterates through 3 pixels, changes least significant bit of RGB binary values
+        for count in range(3):
+
+            # get RGB values for current pixel
+            r, g, b = pixels[w_index, h_index]
+            
+            # convert rgb into binary
+            r_bit = bin(r)
+            g_bit = bin(g)
+            b_bit = bin(b)
+
+            # change least significant bit of each byte
+            if(bit_index < 8):
+                r_bit = r_bit[0 : len(r_bit) - 1] + data[i][bit_index]
+                bit_index += 1
+            
+            if(bit_index < 8):
+                g_bit = g_bit[0 : len(g_bit) - 1] + data[i][bit_index]
+                bit_index += 1
+            
+            if(bit_index < 8):
+                b_bit = b_bit[0 : len(b_bit) - 1] + data[i][bit_index]
+                bit_index += 1
+
+            # save updated pixel
+            pixels[w_index, h_index] = (int(r_bit, 2), int(g_bit, 2) , int(b_bit, 2))
+
+            # update w_index
+            if w_index == (width - 1):
+                w_index = 0
+                h_index += 1
+            else:
+                w_index += 1
+
+    # add trail of zeroes to mark the end of the message
+    # all zeroes is a NULL value, we don't expect it too occur
+    for count in range(10):
+        
+        # set bit_index to 0
+        bit_index = 0
+        
+        for count in range(3):
+
+            # get RGB values for current pixel
+            r, g, b = pixels[w_index, h_index]
+            
+            # convert rgb into binary
+            r_bit = bin(r)
+            g_bit = bin(g)
+            b_bit = bin(b)
+
+            # change least significant bit of each byte
+            if(bit_index < 8):
+                r_bit = r_bit[0 : len(r_bit) - 1] + "0"
+                bit_index += 1
+            
+            if(bit_index < 8):
+                g_bit = g_bit[0 : len(g_bit) - 1] + "0"
+                bit_index += 1
+            
+            if(bit_index < 8):
+                b_bit = b_bit[0 : len(b_bit) - 1] + "0"
+                bit_index += 1
+
+            # save updated pixel
+            pixels[w_index, h_index] = (int(r_bit, 2), int(g_bit, 2) , int(b_bit, 2))
+
+            # update w_index
+            if w_index == (width - 1):
+                w_index = 0
+                h_index += 1
+            else:
+                w_index += 1
+
+    # return encoded image
+    return enc_image
+
+# -------------------------------------------------------------------------------------------
+
+# call main fxn
+if __name__ == '__main__' :
+    main()
