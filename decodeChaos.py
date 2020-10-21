9197244faf8914be26666957754a4c4be706c289
malqatam <malqatam@macalester.edu>
2020-10-20 19:12:53 -0500

    Encode/decode fxns for chaos implementations. Still buggy: encryption throws error

---
+++ decodeChaos.py
@@ -0,0 +1,111 @@
+# decodeLSB.py
+#
+# contains methods for decoding a message in an image
+#
+# -------------------------------------------------------------------------------------------
+
+# imports
+from PIL import Image
+from process import *
+import cv2 as cv
+
+# -------------------------------------------------------------------------------------------
+
+
+
+
+
+# main
+def main():
+    # take key input
+    print("\nProvide the key used to encrypt the message:")
+    key = str(input())
+
+    # take image input
+    # print("Enter the name of the image file (include extension)")
+    # image_file = input()
+    image_file = "encodedCat.png"
+    image = Image.open(image_file, 'r')
+
+    # decode message from image with key
+    message = decode(image, key)
+
+    # print decoded message
+    print("\nYour decoded message is: " + message)
+
+
+# -------------------------------------------------------------------------------------------
+
+# decode
+# decodes a message encoded in an image using the encode function in encodeLSB.py
+### image: image with encoded message
+### key: key used to encode message
+### returns: DecryptedMessage file
+def decode(image, key):
+
+    # copies pixel data for input image
+    image = image.convert('RGB')
+    pixels = image.load()
+
+    edges = cv.Canny(cv.imread('cat.png', 0), 100, 200)  # SHOULD BE IMAGE OR ENC_IMAGE AS PARAM -- still need to resolve later
+
+    edge_pix = []
+
+    for i in range(edges.shape[0]):
+        for j in range(edges.shape[1]):
+            if edges[i, j] != 0:
+                edge_pix.append((i, j))
+
+
+
+    message = ''
+    stop_decoding = False
+    i = 0
+    while(stop_decoding == False):
+        # get RGB values for current edge pixel
+        r, g, b = pixels[edge_pix[i][1], edge_pix[i][0]]
+
+
+        # convert rgb into binary
+        r_bit = bin(r)
+        g_bit = bin(g)
+        b_bit = bin(b)
+
+        a1 = int(r_bit[len(r_bit) - 1])
+        a2 = int(g_bit[len(g_bit) - 1])
+        a3 = int(b_bit[len(b_bit) - 1])
+
+        message = message + str(a1 ^ a3) + str(a2 ^ a3)
+
+        i += 1
+
+
+
+        # check for 16 zeros signifying end of message
+        if len(message) >= 16:
+            if (message[(len(message) - 16):(len(message))] == '0000000000000000') & (len(message)%8 == 0):
+                stop_decoding = True
+
+    # drop 16 zeros signifying end of msg
+    message = message[0:(len(message) - 16)]
+
+    # convert binary message to binary list
+    bin_list = []
+    for i in range(int(len(message)/8)):
+        bin_list.append(message[8*i : 8*i + 8])
+
+    #print(bin_list)
+
+    # decrypt message @Aron
+    message = DecryptedMessage(bin_list, key)
+
+
+
+    # return message
+    return message.getString()
+
+# -------------------------------------------------------------------------------------------
+
+# call main fxn
+if __name__ == '__main__' :
+    main()
--- encodeChaos.py
+++ encodeChaos.py
@@ -18,8 +18,9 @@ def main():
     # take message input
     print("\nEnter the message to encode:")
 
-    # encrypt message
+    # encrypt message @Aron
     encrypted_msg = EncryptedMessage(str(input()))
+    #print(encrypted_msg.getString())
 
     # print key
     key = encrypted_msg.getKey()
@@ -31,7 +32,7 @@ def main():
     image_file = "cat.png"
     image = Image.open(image_file, 'r')
 
-    # encode encrypted message in image
+    # encode encrypted message in image @Aron
     enc_image = encodeChaos(image, encrypted_msg.getBinList())
 
     # save encoded image
@@ -43,40 +44,38 @@ def main():
 
 # -------------------------------------------------------------------------------------------
 
-# def stringToBinary(str):
-#  res = ' '.join(format(ord(i), 'b') for i in str)
-#  return res
-
-
-# -------------------------------------------------------------------------------------------
-
 # encodeChaos
 # encodes a list of binary values into the edge pixels of an image using Canny edge detection
 # uses half as many pixels as LSB
 ### image: image object
 
 def encodeChaos(image, data):
-    # copies input image
+    # copies input image, convert to RGB
     enc_image = image.copy()
     enc_image = enc_image.convert('RGB')
 
-    #msg = "hello"
-    msg = data   # msg = stringToBinary(msg).split()
+    #print(data)
+
+    msg = data
     data = ''
 
     for byte in msg:
         data = data + byte
 
+    # mark the end of the msg with 16 zeros
+    data = data + '0000000000000000'
+
+    #determines number of pix necessary for encoding
     pixnum = int(len(data)/2)
-    # img = cv.imread('cat.png',0)
-    edges = cv.Canny(cv.imread('cat.png',0),100,200) #SHOULD BE IMAGE OR ENC_IMAGE AS PARAM
 
+    #selects edge pix for encodeding, adds to list
+    edges = cv.Canny(cv.imread('cat.png',0),100,200) #SHOULD BE IMAGE OR ENC_IMAGE AS PARAM -- Still need to resolve later
     edge_pix = []
-
     for i in range(edges.shape[0]):
         for j in range(edges.shape[1]):
             if edges[i,j] != 0:
                 edge_pix.append((i,j))
+
 
     # copies pixel data for input image
     pixels = enc_image.load()
@@ -84,7 +83,7 @@ def encodeChaos(image, data):
     # initializes variables for loop
     k = 0
 
-    for i in range(pixnum - 1):
+    for i in range(pixnum):
         # get RGB values for current edge pixel
         r, g, b = pixels[edge_pix[i][1], edge_pix[i][0]]
 
@@ -97,17 +96,18 @@ def encodeChaos(image, data):
         a2 = int(g_bit[len(g_bit)-1])
         a3 = int(b_bit[len(b_bit)-1])
 
+
         x1 = int(data[k])
         x2 = int(data[k + 1])
 
-        if x1 == (a1^a3) & x2 == (a2^a3):
-            continue
-        elif x1 == (a1^a3) & x2 != (a2^a3):
-            g_bit = g_bit[len(g_bit)-1] + str((a2 + 1)%2)
-        elif x1 != (a1^a3) & x2 == (a2^a3):
-            r_bit = r_bit[len(r_bit)-1] + str((a2 + 1)%2)
-        elif x1 != (a1^a3) & x2 != (a2^a3):
-            b_bit = b_bit[len(b_bit)-1] + str((a2 + 1)%2)
+        if (x1 == (a1^a3)) & (x2 == (a2^a3)):
+            pass
+        elif (x1 == (a1^a3)) & (x2 != (a2^a3)):
+            g_bit = g_bit[0 : len(g_bit) - 1] + str((a2 + 1)%2)
+        elif (x1 != (a1^a3)) & (x2 == (a2^a3)):
+            r_bit = r_bit[0 : len(r_bit) - 1] + str((a1 + 1)%2)
+        elif (x1 != (a1^a3)) & (x2 != (a2^a3)):
+            b_bit = b_bit[0 : len(b_bit) - 1] + str((a3 + 1)%2)
         else:
             print('ERROR')
 
